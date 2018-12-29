bb0804e18eef0b936e7edb006e6e659e73c3669f
wangxin <940256527@qq.com>
2018-12-29 18:54:54 +0800

    。

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,11 @@
       <div class="logo">LOGO</div>
     </div>
     <div class="body">
-      <div class="left"></div>
+      <div class="left">
+        <ul class="menu">
+          <li><a href="">权限管理</a></li>
+        </ul>
+      </div>
       <div class="right">
         <p>11111111111111111我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页我是主页</p>
       </div>
@@ -32,6 +36,9 @@ export default {
   .header {height: 60px;width: 100%;background: #23262E}
   .header .logo{width: 200px;height: 60px;line-height: 60px;text-align: center;font-size: 24px;color: #fff}
   .body {width: 100%;position: fixed;top: 60px;left: 0;right: 0;bottom: 0;}
-  .left{width: 200px;float: left;background: #393D49;height: 100%;}
-  .right{padding: 10px;height: 100%;background: #f1f1f1;overflow: hidden;}
+  .body .left{width: 200px;float: left;background: #393D49;height: 100%;}
+  .body .left .menu{margin: 0;padding: 0;list-style: none}
+  .body .left .menu li{line-height: 46px;height: 46px;text-align: center;}
+  .body .left .menu li a{color: #fff;list-style: none;text-decoration: none}
+  .body .right{padding: 10px;height: 100%;background: #f1f1f1;overflow: hidden;}
 </style>
